e86b2de7a034a3ccbe761a07de180b833c38d0dd
Tam Bui <itsmetambui@gmail.com>
2019-02-16 08:31:12 +0700

    feat: add formik validation

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,32 +1,61 @@
 import React, { useState } from 'react';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
+
 import client from './feathers';
 
 const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState(null);
-
-  const login = async () => {
-    try {
-      await client.authenticate({strategy: 'local', email, password});
-    } catch(e) {
-      setError(e);
-    }
-  };
-
-  const signup = async () => {
-    await client.service('users').create({ email, password });
-    login();
-  };
 
   return (
     <div className="login">
       <h1 className="header-primary">Welcome to vutachat <span role="img" aria-label="emoji-100">💯</span></h1>
-      <form className="login-form">
-        <input type="email" className="login-form__input" name="email" placeholder="Could I have your email address?" onChange={e => setEmail(e.target.value)} value={email} />
-        <button className="login-form__btn"><span role="img" aria-label="emoji-submit">👉</span></button>
-      </form>
-      
+      <Formik
+        initialValues={{ email: '' }}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }, 500);
+        }}
+        validationSchema={Yup.object().shape({
+          email: Yup.string()
+            .email('Are you sure this is a valid email? 🤔')
+            .required('I will not spam your inbox! 😁'),
+        })}
+      >
+        {props => {
+          const {
+            values,
+            touched,
+            errors,
+            isSubmitting,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+          } = props;
+          return (
+            <form className="login-form" onSubmit={handleSubmit}>
+              <input
+                id="email"
+                placeholder="Could I have your email address please?"
+                type="text"
+                value={values.email}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                className={
+                  errors.email && touched.email ? 'login-form__input--error' : 'login-form__input'
+                }
+              />
+              {errors.email &&
+                touched.email && <div className="input-feedback">{errors.email}</div>}
+
+              <button className="login-form__btn" type="submit" disabled={isSubmitting}>
+                <span role="img" aria-label="emoji-submit">👉</span>
+              </button>
+            </form>
+          );
+        }}
+      </Formik>
     </div>
   );
 };
