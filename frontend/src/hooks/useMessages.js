4967c15c82545d03745f5c867018885f5ea6a309
Tam Bui <tam.bui@spacestep.ca>
2019-12-11 09:53:19 +0700

    add sound notification when message arrived

--- frontend/src/hooks/useMessages.js
+++ frontend/src/hooks/useMessages.js
@@ -1,15 +1,21 @@
 import { useState, useEffect } from 'react';
+import UIfx from 'uifx';
 
 import client from '../feathers';
+import ringtoneMp3 from '../assets/ringtone.mp3';
 
 const messageService = client.service('messages');
+const notificationSound = new UIfx(ringtoneMp3);
 
 const useMessages = (initMessages = [], initialRoomId) => {
-  const [ messages, setMessages ] = useState(initMessages);
-  const [ roomId ] = useState(initialRoomId); // TODO: this state not updated with latest state.
+  const [messages, setMessages] = useState(initMessages);
+  const [roomId] = useState(initialRoomId); // TODO: this state not updated with latest state.
 
   const handleMessageCreated = message => setMessages(messages => {
-    if(!roomId || message.roomId === roomId) return [...messages, message];
+    if (!roomId || message.roomId === roomId) {
+      notificationSound.play();
+      return [...messages, message];
+    }
     return messages;
   });
 
