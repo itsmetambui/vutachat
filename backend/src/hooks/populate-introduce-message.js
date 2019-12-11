884a425abd37e6a2c828132f8f4f1147b0dbe826
Tam Bui <tam.bui@spacestep.ca>
2019-12-11 09:23:26 +0700

    update introduce messages

--- backend/src/hooks/populate-introduce-message.js
+++ backend/src/hooks/populate-introduce-message.js
@@ -5,6 +5,9 @@ module.exports = function (options = {}) {
     const { app, result } = context;
 
     const master = await app.service('master-user').find();
+    if (master === null) {
+      return context;
+    }
     const roomId = result.rooms[0];
 
     let now = new Date().getTime();
@@ -20,7 +23,7 @@ module.exports = function (options = {}) {
 
     try {
       app.service('messages').create(messages);
-    } catch(e) {
+    } catch (e) {
       throw e;
     }
 
@@ -30,12 +33,12 @@ module.exports = function (options = {}) {
 
 const introduceMessages = [
   `My name is Tam.`,
-  `I'm a Javascript Fullstack Developer with 3 years of experiences in Web development.
-   I speak HTML/CSS, React, Angular, Node and well versed in other libraries like Redux, Feathersjs, and Loopback.
+  `I'm a Fullstack Developer with 3 years of experiences in Web development.
+   I speak HTML/CSS, React, Angular, Node and well versed in other libraries like Redux, Nestjs, and Loopback.
    TDD development, CI/CD and Containerization are also there in my toolbox.`,
   `I love being part of a team, working in an agile environment, together toward a goal.
    I love to take initiative and ownership. I am always thriving for growth as I am an early adopter for new concepts like Suspense, Hooks, react-cache…
    I love to put my new knowledge to use on side projects before applying them for real works.
-   You can check out my works at itsmetambui.now.sh .`,
+   You can check out my works at itsmetambui.netlify.sh .`,
   `If you have a position available, I'd love to hear from you, please shoot me a message :).`
 ];
